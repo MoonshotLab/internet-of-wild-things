9836a4485bbfc924c73dd43ac49530adc99767dd
Joe Longstreet <joelongstreet@gmail.com>
2014-04-14 09:55:55 -0500

    'namespace' js methods

--- public/app.js
+++ public/app.js
@@ -2,60 +2,64 @@ var socket = io.connect('/');
 
 $(function(){
   // Page - Core
-  $('#add-webhook').click(function(){
-    var url = $('#webhook-input').val();
-    var pinId = $('#webhook-pin').val();
-    if(!url) alert('Add a URL');
-    else{
-      socket.emit('create-webhook', {
-        coreId: core.coreId,
-        pinId: pinId,
-        webhookUrl: url
-      });
+  if$('.container.main').hasClass('page-core'){
+    $('#add-webhook').click(function(){
+      var url = $('#webhook-input').val();
+      var pinId = $('#webhook-pin').val();
+      if(!url) alert('Add a URL');
+      else{
+        socket.emit('create-webhook', {
+          coreId: core.coreId,
+          pinId: pinId,
+          webhookUrl: url
+        });
 
-      var template = [
-        "<li class='list-group-item'>",
-          pinId,
-          " - ",
-          url,
-        "</li>"
-      ].join('');
+        var template = [
+          "<li class='list-group-item'>",
+            pinId,
+            " - ",
+            url,
+          "</li>"
+        ].join('');
 
-      $('#hooks-list').append(template);
-    }
-  });
-
+        $('#hooks-list').append(template);
+      }
+    });
+  }
 
   // Page - Control Outputs
-  $('#led-on').click(function(){
-    socket.emit('set-pin-val', {
-      core: core,
-      pinId: 'D0',
-      pinVal: 1
+  if$('.container.main').hasClass('page-control-outputs'){
+    $('#led-on').click(function(){
+      socket.emit('set-pin-val', {
+        core: core,
+        pinId: 'D0',
+        pinVal: 1
+      });
     });
-  });
 
-  $('#led-off').click(function(){
-    socket.emit('set-pin-val', {
-      core: core,
-      pinId: 'D0',
-      pinVal: 0
+    $('#led-off').click(function(){
+      socket.emit('set-pin-val', {
+        core: core,
+        pinId: 'D0',
+        pinVal: 0
+      });
     });
-  });
 
-  $('#servo-move').click(function(){
-    socket.emit('set-pin-val', {
-      core: core,
-      pinId: 'A0',
-      pinVal: $('#servo-input').val()
+    $('#servo-move').click(function(){
+      socket.emit('set-pin-val', {
+        core: core,
+        pinId: 'A0',
+        pinVal: $('#servo-input').val()
+      });
     });
-  });
-
+  }
 
   // Page - Watch Inputs
-  socket.on('input-update', function(opts){
-    if(opts.coreId == core.coreId){
-      $('#' + opts.pinId).text(opts.state);
-    }
-  });
+  if$('.container.main').hasClass('page-watch-inputs'){
+    socket.on('input-update', function(opts){
+      if(opts.coreId == core.coreId){
+        $('#' + opts.pinId).text(opts.state);
+      }
+    });
+  }
 });
